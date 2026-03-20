7791547046fc9368f34c6bd30cb79328533f6aba
alfred154 <alfred@vapecity.us>
2026-03-20 10:10:27 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,21 @@ from discord.ext import commands
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
+intents.message_content = True  # REQUIRED for reading messages
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Tester bot online as {bot.user}")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong! Your Railway deployment works.")
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
 
-bot.run(TOKEN)
+    # If the bot is tagged in the message
+    if bot.user in message.mentions:
+        await message.channel.send("You tagged me! I'm alive on Railway.")
+
+    await bot.process_commands(message)
